f2fa9c2cbf00fd07c3369ee202fb6a98b153a58d
Andrew Mussey <MusseyAM@vt.edu>
2014-01-06 17:52:20 -0500

    Added the ability to add a new team.

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,5 @@
+Array.prototype.last = function() {return this[this.length-1];}
+
 function log_user_in(highlight_info) {
     alert("fetching bg page");
     var bg = chrome.extension.getBackgroundPage();
@@ -6,13 +8,31 @@ function log_user_in(highlight_info) {
 
 highlight_info = JSON.parse(localStorage["json"]);
 
-for (var i = 0; i < highlight_info.length; i++) {
-    $("#loaded_teams").append("<h3>" + highlight_info[i]["name"] + " \
+function load_team(highlight_info) {
+    $("#loaded_teams").append("<h3>" + highlight_info["name"] + " \
         <input class=\"color_boxes color\" team=\"" + i + "\" style=\"background-color:" + 
-        highlight_info[i]["color"] + "\" value=\"" + rgbaStringToHex(highlight_info[i]["color"]) + "\">&nbsp;</div></h3> \
-        <input type=\"textbox\" value=\"" + highlight_info[i]["members"].join(",") +
+        highlight_info["color"] + "\" value=\"" + rgbaStringToHex(highlight_info["color"]) + "\">&nbsp;</div></h3> \
+        <input type=\"textbox\" value=\"" + highlight_info["members"].join(",") +
         "\" style=\"width:100%\" class=\"team_members\" team=\"" + i +
         "\"><br />");
+}
+
+function add_team(team_name) {
+
+
+    loaded_json = JSON.parse(localStorage["json"]);
+    loaded_json.push({"name":team_name,"members":[],"color":"rgba(255, 255, 255, 0.37)"});
+    localStorage["json"] = JSON.stringify(loaded_json);
+
+    load_team(loaded_json.last());
+
+    var bg = chrome.extension.getBackgroundPage();
+    bg.update_team_page();
+}
+
+
+for (var i = 0; i < highlight_info.length; i++) {
+    load_team(highlight_info[i]);
 }
 
 $(".login").click(function(){
@@ -39,7 +59,7 @@ $(".team_members").change(function() {
 
 $(".color_boxes").change(function() {
 
-    index_of_item = parseInt($(this).attr("team"))
+    index_of_item = parseInt($(this).attr("team"));
     loaded_json = JSON.parse(localStorage["json"]);
     loaded_json[index_of_item]["color"] = hexToRgbaString($(this).val(), 0.37);
     localStorage["json"] = JSON.stringify(loaded_json);
@@ -47,4 +67,10 @@ $(".color_boxes").change(function() {
 
     var bg = chrome.extension.getBackgroundPage();
     bg.update_team_page();
+});
+
+
+$("#add_team_button").click(function() {
+    add_team($("#add_team_text").val());
+    $("#add_team_text").val("");
 });
